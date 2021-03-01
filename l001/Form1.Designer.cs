b5acef32ec3f8d54846fc4205c7a120eb607dfc9
Eduard1212 <alfasports22@gmail.com>
2021-03-01 15:02:15 +0700

    Mutovin Eduard PR-395

---
+++ l001/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace WSMutovin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtMiddleName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnExit);
+            this.panel1.Controls.Add(this.btnDelete);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.Clear);
+            this.panel1.Controls.Add(this.btnCreate);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel1.Location = new System.Drawing.Point(478, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(150, 476);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.FlatAppearance.BorderSize = 0;
+            this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExit.Font = new System.Drawing.Font("Century Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(109, 3);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(38, 23);
+            this.btnExit.TabIndex = 12;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.FlatAppearance.BorderSize = 0;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Location = new System.Drawing.Point(3, 254);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(145, 102);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpdate.FlatAppearance.BorderSize = 0;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Location = new System.Drawing.Point(3, 142);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(145, 102);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // Clear
+            // 
+            this.Clear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Clear.FlatAppearance.BorderSize = 0;
+            this.Clear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Clear.Location = new System.Drawing.Point(3, 366);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(145, 102);
+            this.Clear.TabIndex = 1;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCreate.FlatAppearance.BorderSize = 0;
+            this.btnCreate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCreate.Location = new System.Drawing.Point(3, 30);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(145, 102);
+            this.btnCreate.TabIndex = 0;
+            this.btnCreate.Text = "Create";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGrid);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.panel2.Location = new System.Drawing.Point(0, 287);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(478, 189);
+            this.panel2.TabIndex = 1;
+            // 
+            // dataGrid
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.dataGrid.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
+            this.dataGrid.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGrid.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.dataGrid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGrid.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGrid.Location = new System.Drawing.Point(0, 0);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.ReadOnly = true;
+            this.dataGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGrid.Size = new System.Drawing.Size(478, 189);
+            this.dataGrid.TabIndex = 0;
+            this.dataGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGrid_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Фамилия:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Почта:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Имя:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(33, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 21);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Телефон:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(33, 130);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 21);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Отчество:";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtFirstName.Location = new System.Drawing.Point(185, 29);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(215, 20);
+            this.txtFirstName.TabIndex = 7;
+            // 
+            // txtMiddleName
+            // 
+            this.txtMiddleName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtMiddleName.Location = new System.Drawing.Point(185, 80);
+            this.txtMiddleName.Name = "txtMiddleName";
+            this.txtMiddleName.Size = new System.Drawing.Size(215, 20);
+            this.txtMiddleName.TabIndex = 8;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLastName.Location = new System.Drawing.Point(185, 131);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(215, 20);
+            this.txtLastName.TabIndex = 9;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtPhone.Location = new System.Drawing.Point(185, 182);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(215, 20);
+            this.txtPhone.TabIndex = 10;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtEmail.Location = new System.Drawing.Point(185, 232);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(215, 20);
+            this.txtEmail.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
+            this.ClientSize = new System.Drawing.Size(628, 476);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.txtMiddleName);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ws";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button btnCreate;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtMiddleName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.DataGridView dataGrid;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ l001/Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.Generic;
+
+namespace WSMutovin
+{
+    public partial class Form1 : Form
+    {
+        public string connect = @"Data Source=DESKTOP-95TFA7J\SQLEXPRESS;Initial Catalog=WSDataBase;Integrated Security=True";
+        public int ClientID;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //load from
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.dataGrid.DefaultCellStyle.ForeColor = Color.FromArgb(62, 120, 138);
+            this.dataGrid.DefaultCellStyle.BackColor = Color.FromArgb(41, 44, 51);
+            this.dataGrid.DefaultCellStyle.SelectionForeColor = Color.FromArgb(41, 44, 51);
+            this.dataGrid.DefaultCellStyle.SelectionBackColor = Color.FromArgb(62, 120, 138);
+
+            getRefresh();
+        }
+
+        //button to exit
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        //enter the client information
+        public void getRefresh()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(connect);
+
+                SqlCommand command = new SqlCommand("SELECT * FROM Clients ORDER BY id", Connection);
+                DataTable data = new DataTable();
+
+                Connection.Open();
+
+                SqlDataReader sdr = command.ExecuteReader();
+                data.Load(sdr);
+
+                Connection.Close();
+
+                dataGrid.DataSource = data;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //the output of the selected row in text boxes
+        private void dataGrid_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            ClientID = Convert.ToInt32(dataGrid.SelectedRows[0].Cells[0].Value);
+
+            txtFirstName.Text = dataGrid.SelectedRows[0].Cells[1].Value.ToString();
+            txtMiddleName.Text = dataGrid.SelectedRows[0].Cells[2].Value.ToString();
+            txtLastName.Text = dataGrid.SelectedRows[0].Cells[3].Value.ToString();
+            txtPhone.Text = dataGrid.SelectedRows[0].Cells[4].Value.ToString();
+            txtEmail.Text = dataGrid.SelectedRows[0].Cells[5].Value.ToString();
+        }
+
+        //create command
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtPhone.Text != "" || txtEmail.Text != "")
+                {
+                    SqlConnection Connection = new SqlConnection(connect);
+                    string Query = "INSERT INTO Clients(firstname, middlename, lastname, phone, email) VALUES " +
+                        "(" +
+                        "'" + txtFirstName.Text + "'," +
+                        "'" + txtMiddleName.Text + "'," +
+                        "'" + txtLastName.Text + "'," +
+                        "'" + txtPhone.Text + "'," +
+                        "'" + txtEmail.Text + "'" +
+                        ")";
+
+                    Connection.Open();
+
+                    SqlCommand command = new SqlCommand(Query, Connection);
+                    command.ExecuteNonQuery();
+
+                    Connection.Close();
+
+                    getRefresh();
+                    Clears();
+                }
+                else
+                {
+                    MessageBox.Show("Введите номер телефона либо адрес электронной почты!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //update command
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ClientID > 0)
+                {
+                    SqlConnection Connection = new SqlConnection(connect);
+                    string Query = "UPDATE Clients SET " +
+                        "firstname = '" + txtFirstName.Text + "'," +
+                        " middlename = '" + txtMiddleName.Text + "'," +
+                        " lastname = '" + txtLastName.Text + "'," +
+                        " phone = '" + txtPhone.Text + "'," +
+                        " email = '" + txtEmail.Text + "'" +
+                        " WHERE " +
+                        "id = '" + this.ClientID + "'";
+                    Connection.Open();
+
+                    SqlCommand command = new SqlCommand(Query, Connection);
+                    command.ExecuteNonQuery();
+
+                    Connection.Close();
+
+                    getRefresh();
+                    Clears();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //delete command
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ClientID > 0)
+                {
+                    if (MessageBox.Show("Вы действительно хотите удалить данного клиента?", "Удаление", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        SqlConnection Connection = new SqlConnection(connect);
+                        string Query = "DELETE Clients WHERE id = '" + this.ClientID + "'";
+
+                        Connection.Open();
+
+                        SqlCommand command = new SqlCommand(Query, Connection);
+                        command.ExecuteNonQuery();
+
+                        Connection.Close();
+
+                        getRefresh();
+                        Clears();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Пожалуйста, выберите клиента!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //clear command
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            Clears();
+        }
+
+        //method that clears text fields
+        private void Clears()
+        {
+            txtEmail.Clear();
+            txtFirstName.Clear();
+            txtLastName.Clear();
+            txtMiddleName.Clear();
+            txtPhone.Clear();
+
+            ClientID = 0;
+            try
+            {
+                string connect = @"Data Source=DESKTOP-CSSH9GB\SQLEXPRESS;Initial Catalog=MVC;Integrated Security=True";
+                SqlConnection con = new SqlConnection(connect);
+                con.Open();
+                SqlCommand cmd = con.CreateCommand();
+                cmd.CommandText = "SELECT Name FROM [Users]";
+                string fio = Convert.ToString(cmd.ExecuteScalar());
+                this.label1.Text += fio;
+                cmd.CommandText = "SELECT Surname FROM [Users]";
+                fio = Convert.ToString(cmd.ExecuteScalar());
+                this.label1.Text += fio;
+                cmd.CommandText = "SELECT Patronymic FROM [Users]";
+                fio = Convert.ToString(cmd.ExecuteScalar());
+                this.label1.Text += fio;
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(Convert.ToString(ex), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
